d5ae66f75df8554f073b3e3171385953bcdf52af
unknown <dlorenzo.05@gmail.com>
2020-10-19 16:53:17 +0200

    rangeInput inported and added to App function

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-// const App = () => (
-//   <Header />
-// );
+import RangeInput from './components/range-input';
+
+const App = () => (
+  <RangeInput />
+);
 
 export default App;

466643e5930c008dcb35cb5c56208949f9a2e273
Katia-2020 <kateflights@gmail.com>
2020-10-20 14:33:53 +0100

    fixed index and app files

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RangeInput from './components/range-input';
 
-const App = () => (
-  <RangeInput />
-);
+const App = () => {
+  const [values, setValues] = useState({});
+
+  const handleChange = (values) => {
+
+    setValues(values);
+  };
+
+
+  console.log(values);
+
+  return (
+    <>
+      <RangeInput onChange={handleChange} />
+
+      {Object.entries(values).map(([key, value]) => {
+        return (
+          <div>{key}: {value}</div>
+        )
+      })}
+    </>
+  )
+};
 
 export default App;
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './reset.scss';
 import App from './app';
 
 const rootElement = document.getElementById('app');

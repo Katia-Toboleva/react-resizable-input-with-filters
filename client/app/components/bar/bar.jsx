bceed021e5ec25b983efa3f040c5cd8cafa833e8
Katia-2020 <kateflights@gmail.com>
2020-10-21 12:57:45 +0100

    added props to bar

--- client/app/components/bar/bar.jsx
+++ client/app/components/bar/bar.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import styles from './bar.scss';
 
 const Bar = (props) => {
-  const { right, left } = props;
+  const {
+    right,
+    left,
+    type,
+    onMouseDown,
+    width,
+  } = props;
 
   const barLeftAndRightStyles = {
     left: `${left}px`,
     right: `${right}px`,
-    width: `(${window.innerWidth} - ${left + right})px`,
+    width: `${width}%`,
+  };
+
+  const handleMouseDown = () => {
+    onMouseDown(type);
   };
 
   return (
     <div
+      type={type}
       className={styles['range-input-bar']}
       style={barLeftAndRightStyles}
+      onMouseDown={handleMouseDown}
     />
   );
 };

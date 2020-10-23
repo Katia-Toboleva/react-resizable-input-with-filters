8708a20c7441c8fe743164e7fa4916e0d656453d
Katia-2020 <kateflights@gmail.com>
2020-10-23 19:44:43 +0100

    updated logic in scale to get correct width of each space

--- client/app/components/scale/scale.jsx
+++ client/app/components/scale/scale.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './scale.scss';
 
 const getStyles = (spaces) => ({
-  width: `${spaces}%`,
+  width: `${100/spaces}%`,
 });
 
 const Scale = (props) => {
   const { spaces } = props;
   return (
     <div className={styles['scale']}>
-      {[...Array(100 / spaces)].map((item, index) => (
+      {[...Array(spaces)].map((item, index) => (
         <div key={index} className={styles['scale__item']} style={getStyles(spaces)} />
       ))}
     </div>

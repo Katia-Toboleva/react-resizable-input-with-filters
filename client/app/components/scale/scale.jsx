334da22b793be1491587a674b992d7c6def0cb5d
Katia-2020 <kateflights@gmail.com>
2020-10-23 15:57:32 +0100

    added dynamic array

--- client/app/components/scale/scale.jsx
+++ client/app/components/scale/scale.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import styles from './scale.scss';
 
-const arr = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-];
+const getStyles = (spaces) => ({
+  width: `${spaces}%`,
+});
 
-const stylesScale = {
-  width: `${100 / arr.length}%`,
+const Scale = (props) => {
+  const { spaces } = props;
+  return (
+    <div className={styles['scale']}>
+      {[...Array(100 / spaces)].map((item, index) => (
+        <div key={index} className={styles['scale__item']} style={getStyles(spaces)} />
+      ))}
+    </div>
+  );
 };
-
-const Scale = () => (
-
-  <div className={styles['scale']}>
-    {arr.map((num) => (
-      <div key={num} className={styles['scale__item']} style={stylesScale} />
-    ))}
-  </div>
-);
 
 export default Scale;

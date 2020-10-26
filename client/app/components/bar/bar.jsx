4261c677ee4903728fb08d4f9ddc19b58a94de1c
Katia-2020 <kateflights@gmail.com>
2020-10-26 10:06:56 +0000

    added theme on bar change

--- client/app/components/bar/bar.jsx
+++ client/app/components/bar/bar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import styles from './bar.scss';
+
+const cx = classnames.bind(styles);
 
 const Bar = (props) => {
   const {
@@ -14,16 +17,25 @@ const Bar = (props) => {
     left: `${left}%`,
     right: `${right}%`,
     width: `${width}%`,
+    // background: `linear-gradient(90deg, rgb(2${left}, 1${left}, ${left}), rgb(${right}, 1${right}, 2${right}))`,
   };
 
   const handleMouseDown = () => {
     onMouseDown(type);
   };
 
+  const getClassnames = () => cx('range-input-bar', {
+    'range-input-bar--gradient-blue-red': left === 10 || right === 10,
+    'range-input-bar--gradient-green-yellow': left === 20 || right === 20,
+    'range-input-bar--gradient-orange-red': left === 30 || right === 30,
+    'range-input-bar--gradient-purple-pink': left === 40 || right === 40,
+    'range-input-bar--gradient-yellow-blue': left === 50 || right === 50,
+  });
+
   return (
     <div
       type={type}
-      className={styles['range-input-bar']}
+      className={getClassnames()}
       style={barLeftAndRightStyles}
       onMouseDown={handleMouseDown}
     />

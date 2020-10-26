1904f3bf15b20d6a3fa1719d5d9cef2284070bcd
Katia-2020 <kateflights@gmail.com>
2020-10-26 23:22:09 +0000

    created Products component

---
+++ client/app/components/products/products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './products.scss';
+
+const Products = () => {
+
+  return (
+    <div className={styles['scale']}/>
+  );
+};
+
+export default Products;

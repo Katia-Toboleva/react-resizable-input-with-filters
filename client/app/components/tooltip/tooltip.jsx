4a626b5bf8aff23e8e2ee980b1209b8cb8009bc8
Katia-2020 <kateflights@gmail.com>
2020-10-26 22:04:15 +0000

    tooltip component created

---
+++ client/app/components/tooltip/tooltip.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './tooltip.scss';
+
+const cx = classnames.bind(styles);
+
+const Tooltip = (props) => {
+  const {
+    type, right, left, active, text,
+  } = props;
+
+  const tooltipStyles = {
+    left: `${left}%`,
+    right: `${right}%`,
+  };
+
+  return (
+    <div
+      type={type}
+      className={cx('tooltip', {
+        [`tooltip--type-${type}`]: type,
+        'tooltip--active': active,
+      })}
+      style={tooltipStyles}
+    >
+      <span className={styles.['tooltip__text']}>
+        {text}
+      </span>
+    </div>
+
+  );
+};
+
+export default Tooltip;

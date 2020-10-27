4274c92c8c7732206c1ead51d334f0bd13cbde0d
Katia-2020 <kateflights@gmail.com>
2020-10-27 22:58:23 +0000

    got rid of inline styles in Tooltip, since after moving the component inside the parent, they were not needed

--- client/app/components/tooltip/tooltip.jsx
+++ client/app/components/tooltip/tooltip.jsx
@@ -6,22 +6,15 @@ const cx = classnames.bind(styles);
 
 const Tooltip = (props) => {
   const {
-    type, right, left, active, text,
+    type, active, text,
   } = props;
-
-  const tooltipStyles = {
-    left: `${left}%`,
-    right: `${right}%`,
-  };
 
   return (
     <div
-      type={type}
       className={cx('tooltip', {
         [`tooltip--type-${type}`]: type,
         'tooltip--active': active,
       })}
-      style={tooltipStyles}
     >
       <span className={styles['tooltip__text']}>
         {text}

e1eb277415d0c8798763e01aaac9dd0e1c4b6af2
Katia-2020 <kateflights@gmail.com>
2020-10-28 21:40:51 +0000

    added image to products

--- client/app/components/products/products.jsx
+++ client/app/components/products/products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './products.scss';
 import { Row, Column } from '../grid';
+import Image from '../image';
 
 const Products = (props) => {
   const { items } = props;
@@ -9,13 +10,18 @@ const Products = (props) => {
     <div className={styles['products']}>
       {items.map((product, index) => (
         <div className={styles['products__item']} key={index}>
-          <Row direction="row" position="space-between">
-            <Column grow>
+          <Row direction="row">
+            <Column grow flex="2">
               <div className={styles['products__name']}>
                 {product.name}
               </div>
             </Column>
-            <Column shrink>
+            <Column grow flex="2">
+              <div className={styles['products__image']}>
+                <Image url={product.url} size="medium" />
+              </div>
+            </Column>
+            <Column shrink flex="1">
               <div className={styles['products__price']}>
                 {`£${product.price}`}
               </div>

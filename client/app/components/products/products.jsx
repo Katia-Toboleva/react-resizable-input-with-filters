2c9b684715175dbd0ac1cf792f71631c58393b9f
Katia-2020 <kateflights@gmail.com>
2020-10-28 15:14:29 +0000

    updated Products component with structure

--- client/app/components/products/products.jsx
+++ client/app/components/products/products.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import styles from './products.scss';
+import { Row, Column } from '../grid';
 
-const Products = () => {
-
+const Products = (props) => {
+  const { items } = props;
+  // console.log(props);
   return (
-    <div className={styles['scale']}/>
+    <div className={styles['products']}>
+      {items.map((product) => (
+        <Row direction="row">
+          <Column grow>
+            <div className={styles['products__name']}>
+              {product.name}
+            </div>
+          </Column>
+          <Column shrink>
+            <div className={styles['products__price']}>
+              {`£${product.price}`}
+            </div>
+          </Column>
+        </Row>
+      ))}
+
+
+    </div>
+
   );
 };
 

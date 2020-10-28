0269f3a871222580cc68923bb0518f732f16c493
Katia-2020 <kateflights@gmail.com>
2020-10-28 18:30:02 +0000

    added products__item box in Products component

--- client/app/components/products/products.jsx
+++ client/app/components/products/products.jsx
@@ -7,19 +7,22 @@ const Products = (props) => {
   // console.log(props);
   return (
     <div className={styles['products']}>
-      {items.map((product) => (
-        <Row direction="row">
-          <Column grow>
-            <div className={styles['products__name']}>
-              {product.name}
-            </div>
-          </Column>
-          <Column shrink>
-            <div className={styles['products__price']}>
-              {`£${product.price}`}
-            </div>
-          </Column>
-        </Row>
+      {items.map((product, index) => (
+        <div className={styles['products__item']} key={index}>
+          <Row direction="row" position="space-between">
+            <Column grow>
+              <div className={styles['products__name']}>
+                {product.name}
+              </div>
+            </Column>
+            <Column shrink>
+              <div className={styles['products__price']}>
+                {`£${product.price}`}
+              </div>
+            </Column>
+          </Row>
+        </div>
+
       ))}
 
 

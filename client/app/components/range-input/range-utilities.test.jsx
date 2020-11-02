0fd6161c1a0ae25e6287cc22ce5c02d03b9a13c2
Katia-2020 <kateflights@gmail.com>
2020-11-02 13:44:53 +0000

    added a test for getMagnetValue from RangeUtilities

--- client/app/components/range-input/range-utilities.test.jsx
+++ client/app/components/range-input/range-utilities.test.jsx
@@ -5,8 +5,8 @@ import * as RangeUtilities from './range-utilities';
 
 describe('RangeUtilities', () => {
   describe('getCoordinates', () => {
-    describe('when called', () => {
-      it('should return correct obj with valid props passed', () => {
+    describe('when called with valid props passed', () => {
+      it('should return correct obj', () => {
         const props = {
           minPrice: 50,
           maxPrice: 250,
@@ -34,6 +34,30 @@ describe('RangeUtilities', () => {
         const expected = {};
 
         expect(received).toMatchObject(expected);
+      });
+    });
+  });
+
+  describe('getMagnetValue', () => {
+    describe('when called', () => {
+      it('should return a correctly rounded value', () => {
+        const percentage = 48;
+        const spaces = 10;
+
+        const received = RangeUtilities.getMagnetValue(percentage, spaces);
+        const expected = 50;
+
+        expect(received).toEqual(expected);
+      });
+
+      it('should return a correctly rounded value', () => {
+        const percentage = 41;
+        const spaces = 10;
+
+        const received = RangeUtilities.getMagnetValue(percentage, spaces);
+        const expected = 40;
+
+        expect(received).toEqual(expected);
       });
     });
   });

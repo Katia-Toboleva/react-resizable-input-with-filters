c22df5b1fc4d4b0f454a42e4348df5ae5a329575
Katia-2020 <kateflights@gmail.com>
2020-11-08 17:36:54 +0000

    fixed eslint issues for Bar component

--- client/app/components/bar/bar.jsx
+++ client/app/components/bar/bar.jsx
@@ -24,11 +24,11 @@ const Bar = (props) => {
   };
 
   const getClassnames = () => cx('range-input-bar', {
-    'range-input-bar--gradient-blue-red': width <= 10 || width > 50 && width <= 60,
-    'range-input-bar--gradient-green-yellow': width > 10 && width <= 20 || width > 60 && width <= 70,
-    'range-input-bar--gradient-orange-red': width > 20 && width <= 30 || width > 70 && width <= 80,
-    'range-input-bar--gradient-purple-pink': width > 30 && width <= 40 || width > 80 && width <= 90,
-    'range-input-bar--gradient-yellow-blue': width > 40 && width <= 50 || width > 90 && width < 100,
+    'range-input-bar--gradient-blue-red': (width <= 10 || width > 50) && width <= 60,
+    'range-input-bar--gradient-green-yellow': (width > 10 && width <= 20) || (width > 60 && width <= 70),
+    'range-input-bar--gradient-orange-red': (width > 20 && width <= 30) || (width > 70 && width <= 80),
+    'range-input-bar--gradient-purple-pink': (width > 30 && width <= 40) || (width > 80 && width <= 90),
+    'range-input-bar--gradient-yellow-blue': (width > 40 && width <= 50) || (width > 90 && width < 100),
   });
 
   return (

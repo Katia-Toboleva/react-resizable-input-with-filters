811dc43d40f8a3f8cad91674f80b6791e3f729fc
Katia-2020 <kateflights@gmail.com>
2020-11-23 15:27:43 +0000

    restructured Header

--- client/app/components/header/header.jsx
+++ client/app/components/header/header.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './header.scss';
 import { Row, Column } from '../grid';
-import Navbar from '../navbar';
+import Button from '../button';
 import Logo from '../logo';
 
 const Header = () => (
   <div className={styles.header}>
-    <Row direction="row">
-      <Column grow>
+    <Row direction="row" position="space-between">
+      <Column>
+        <div className={styles['header__nav']}>
+          <Button text="home" type="nav" />
+          <Button text="about" type="nav" />
+        </div>
+      </Column>
+      <Column>
         <Logo />
       </Column>
-      <Column shrink>
-        <Navbar />
+      <Column>
+        <div className={styles['header__nav']}>
+          <Button text="sign in" type="nav" />
+          <Button text="deals" type="nav" />
+        </div>
       </Column>
     </Row>
   </div>
